1866f38d3886d87f4008aafac3eb35c8df1eab29
Roi Driscoll <roi.driscoll@gmail.com>
2015-05-11 00:29:29 +0100

    Too tired to see.

--- lib/marvel_api.rb
+++ lib/marvel_api.rb
@@ -1,13 +1,30 @@
+require "httparty"
+
 class MarvelApi
+
+  include HTTParty
+
+  base_uri "http://gateway.marvel.com/v1/public"
+  format :json
 
   PUBLIC_KEY  = ENV["MARVEL_PUBLIC_KEY"]
   PRIVATE_KEY = ENV["MARVEL_PRIVATE_KEY"]
 
-  def timestamp
-    Time.now.to_s.gsub(" ","-")
-  end
+  class << self
+    def timestamp
+      Time.now.to_i.to_s
+    end
 
-  def serverside_signature
-    timestamp + PRIVATE_KEY + PUBLIC_KEY
+    def serverside_signature(timestamp)
+      Digest::MD5.hexdigest(timestamp + PRIVATE_KEY + PUBLIC_KEY)
+    end
+
+    def comics
+      get("/comics", query: {
+        ts: timestamp,
+        apikey: PUBLIC_KEY,
+        hash: serverside_signature(timestamp)
+      })
+    end
   end
 end
--- spec/marvel_api_spec.rb
+++ spec/marvel_api_spec.rb
@@ -6,10 +6,12 @@ describe MarvelApi do
 
     let(:md5_signature) { "02b238a7fb81498ba202b238a7fb81498ba2" }
 
-    it "requires a timestamp generated as a string" do
-      marvel = MarvelApi.new
+    it "requires a timestamp" do
+      marvel = class_double("MarvelApi")
 
-      expect(marvel.timestamp).to start_with("20")
+      expect(marvel).to receive(:timestamp).and_return("1431300440")
+
+      marvel.timestamp
     end
 
     it "requires access to a public key" do
@@ -21,11 +23,18 @@ describe MarvelApi do
     end
 
     it "must generate an md5 digest from the timestamp and keys" do
-      marvel = instance_double("MarvelApi")
+      marvel = class_double("MarvelApi")
 
       expect(marvel).to receive(:serverside_signature).and_return(md5_signature)
 
-      marvel.serverside_signature
+      marvel.serverside_signature("timestamp")
+    end
+  end
+
+  xcontext "when accessing 'Comics' resource" do
+
+    it "can retrieve a list of all comics" do
+      expect(MarvelApi.comics).to include("\"code\" => 200,")
     end
   end
 end
