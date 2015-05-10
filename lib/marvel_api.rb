111aa4a483a950a78b8c5e93be2350d962b3de59
Roi Driscoll <roi.driscoll@gmail.com>
2015-05-10 21:01:14 +0100

    Adds server-side authentification requirements

--- lib/marvel_api.rb
+++ lib/marvel_api.rb
@@ -1,8 +1,13 @@
 class MarvelApi
 
-  PUBLIC_KEY = ENV["MARVEL_PUBLIC_KEY"]
+  PUBLIC_KEY  = ENV["MARVEL_PUBLIC_KEY"]
+  PRIVATE_KEY = ENV["MARVEL_PRIVATE_KEY"]
 
   def timestamp
     Time.now.to_s.gsub(" ","-")
+  end
+
+  def serverside_signature
+    timestamp + PRIVATE_KEY + PUBLIC_KEY
   end
 end
--- spec/marvel_api_spec.rb
+++ spec/marvel_api_spec.rb
@@ -4,13 +4,28 @@ describe MarvelApi do
 
   context "in order to authenticate requests" do
 
-    it "must provide a timestamp as a string" do
+    let(:md5_signature) { "02b238a7fb81498ba202b238a7fb81498ba2" }
+
+    it "requires a timestamp generated as a string" do
       marvel = MarvelApi.new
+
       expect(marvel.timestamp).to start_with("20")
     end
 
     it "requires access to a public key" do
       expect(MarvelApi::PUBLIC_KEY).to eq(ENV["MARVEL_PUBLIC_KEY"])
+    end
+
+    it "requires access to a private key" do
+      expect(MarvelApi::PRIVATE_KEY).to eq(ENV["MARVEL_PRIVATE_KEY"])
+    end
+
+    it "must generate an md5 digest from the timestamp and keys" do
+      marvel = instance_double("MarvelApi")
+
+      expect(marvel).to receive(:serverside_signature).and_return(md5_signature)
+
+      marvel.serverside_signature
     end
   end
 end
